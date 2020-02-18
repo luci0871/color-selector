b93e13d5713d9c63b6c333b341da070657cc5f2d
Luciano de Souza <luci0871@stud.kea.dk>
2020-02-18 13:28:39 +0100

    adding javascript scructure

--- color-selector/color.js
+++ color-selector/color.js
@@ -1,3 +1,29 @@
 "use strict"
 
 window.addEventListener("DOMContentLoaded", init);
+
+function init() {
+    showHex()
+}
+
+function showHex() {
+
+    const hexElement = document.querySelector(".colorchange");
+    console.log("showHex");
+
+}
+
+
+/*
+showHex(){
+let hexElement = document.querySelector(".colorchange");
+hexElement.oninput = function() {
+    const hexValue = document.querySelector(ìnput[type=color].value);
+    document.querySelector(".codehex").textContant = `code HEX ${hexvalue}`
+}*/
+
+
+
+/* let x = document.createElement("INPUT");
+    x.setAttribute("type", "color");
+    document.body.appendChild(x); */
